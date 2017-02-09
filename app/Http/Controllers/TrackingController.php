a8945afa14cab0b6fa6be1b2af4b7e59a1bf83df
Wilson Omokoro <wilson.omokoro@andela.com>
2017-02-09 14:23:58 +0100

    create controller

---
+++ app/Http/Controllers/TrackingController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TrackingController extends Controller
+{
+    public function getProducts()
+    {
+
+    }
+
+    public function addProduct()
+    {
+
+    }
+
+    public function findProduct()
+    {
+
+    }
+
+    public function getCategories()
+    {
+
+    }
+
+    public function addCategory()
+    {
+
+    }
+}
