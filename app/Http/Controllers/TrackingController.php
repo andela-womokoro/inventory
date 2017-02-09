278ed072efaf582033c0689ce79086df39cfd794
Wilson Omokoro <wilson.omokoro@andela.com>
2017-02-09 14:39:30 +0100

    code logic for addProduct method

--- app/Http/Controllers/TrackingController.php
+++ app/Http/Controllers/TrackingController.php
@@ -11,9 +11,27 @@ class TrackingController extends Controller
 
     }
 
-    public function addProduct()
+    /**
+     * add a new product to the database
+     * @param Illuminate\Http\Request $request
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function addProduct(Request $request)
     {
+    	$product = Product::create([
+    		'category_id' => $request->category_id,
+    		'name'	=> $request->name,
+    		'description'	=> $request->description,
+    		'price' => $request->price,
+    		'quantity' => $request->quantity,
+    	]);
 
+    	if ($product) {
+    		return response()->json([$product, 200]);
+    	}
+
+    	return response()->json(['Error', 200]);
     }
 
     public function findProduct()
