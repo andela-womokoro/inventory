bc554f73f6fe70fda3588d97af80cbfaf9e77276
Wilson Omokoro <wilson.omokoro@andela.com>
2017-02-09 14:34:04 +0100

    define product model

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'products';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['category_id', 'name', 'description', 'price', 'quantity'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Category');
+    }
 }
